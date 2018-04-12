3f20689fd524396a0989722b53c875014901fed6
Leitino <alessandro.leita89@gmail.com>
2018-04-12 11:44:16 +0200

    filtro funzionante finito

--- main.js
+++ main.js
@@ -10,12 +10,20 @@ $(function() {
           },1000);
       }
   });
-  $('#filter').keypress(function(e) {
-    var messaggio = $('#filter').val()
-    if (e.which == 13) {
-      if ($('.users')(':contains(messaggio)')) {
-        $('.users').show()
+
+  $('#filter').keyup(function() {
+    var search = $('#filter').val();
+    console.log(search);
+    search = search.toLowerCase();
+    var users = $('.users')
+    users.each(function() {
+      var conctatName = $(this).children('.users_text').children('h4')
+      if (conctatName.text().toLowerCase().includes(search)){
+        $(this).show()
       }
-    }
+      else {
+        $(this).hide()
+      }
+    })
   })
 });
